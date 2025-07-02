fae6a128532f04b54b49fe213f672009875becf0
Saleh <theend.s.2002@gmail.com>
2025-07-02 13:35:04 -0700

    cmmE

--- Error.py
+++ Error.py
@@ -3,5 +3,28 @@
 # def hello:
 # return
 # name;;
-
-print(test)
+# ------------------
+# NameError
+# print(test)
+# ------------------
+# type error
+# print(len("saleh"))
+# print(len("5"))
+# "mohammadSaleh" + []
+# ------------------
+# IndexError
+# res = [1,2,3,4]
+# print(res[7])
+# --------------------
+# ValueError
+# int("5")
+# print(int("5"))
+# print(int("fr"))
+# --------------------
+# ket Error
+# bishtar toye dict ha 
+# person = {}
+# print(person["name"])
+# ----------------------
+# AttributeError
+"salehFarahani".test 
