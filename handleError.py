3cdfe0ee9cd48955b1977bb607e61b55a03a4b4d
Saleh <theend.s.2002@gmail.com>
2025-07-05 14:48:58 -0700

    handleE4

--- handleError.py
+++ handleError.py
@@ -1,3 +1,4 @@
+import def
 # try:
 #     print(myName)
 # except:
@@ -35,10 +36,21 @@
 while True:
   try:
       num = int(input('please inter a number: '))
-  except:
+  except :
       print('thats not a number! please enter another one :')
   else:
      print('you have entered a number')
      break
   finally:
      print('this is finally section')
+     
+ #----------------------------------------------
+ def divide(first, second):
+     try:
+         return first / second
+     except ZeroDivisionError:
+         return('Dont divided by zero !!')
+     except TypeError:
+         return('first and second must be num !!!')
+     
+print(divide(10,2))
