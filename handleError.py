f88b74ae417486d1cb60e97b6556a860d8b940f6
Saleh <theend.s.2002@gmail.com>
2025-07-05 11:09:26 -0700

    handleE2

--- handleError.py
+++ handleError.py
@@ -3,12 +3,22 @@ try:
 except:
     print('an error occures')
 #-----------------------------------------
-def get(dic,key):
-    return dic[key]
-    
-print(get(person, 'name'))   
+# def get(dic,key):
+#     return dic[key]
+#-----------------------------------------
+#print(get(person, 'name'))   
 person= {
     "name": "saleh",
     'family':"Fr"
 }
-#print(person["name"])
+
+def get(d,key):
+        try:
+            return d[key]
+        except key: 
+            return "no key found"
+        except IndexError:
+            return 'no indexError'
+
+print(get(person,"name"))
+print(get(person,"age"))
