b8d0c8be6ccaf02d4150c7490e1e0b22af678568
Saleh <theend.s.2002@gmail.com>
2025-07-05 14:25:47 -0700

    handleE3

--- handleError.py
+++ handleError.py
@@ -1,24 +1,33 @@
+# try:
+#     print(myName)
+# except:
+#     print('an error occures')
+# #-----------------------------------------
+# # def get(dic,key):
+# #     return dic[key]
+# #-----------------------------------------
+# #print(get(person, 'name'))   
+# person= {
+#     "name": "saleh",
+#     'family':"Fr"
+# }
+#-------------------------------------------
+# def get(d,key):
+#         try:
+#             return d[key]
+#         except key: 
+#             return "no key found"
+#         except IndexError:
+#             return 'no indexError'
+
+# print(get(person,"name"))
+# print(get(person,"age"))
+#--------------------------------------------
 try:
-    print(myName)
+    num = int(input('please inter a number: '))
 except:
-    print('an error occures')
-#-----------------------------------------
-# def get(dic,key):
-#     return dic[key]
-#-----------------------------------------
-#print(get(person, 'name'))   
-person= {
-    "name": "saleh",
-    'family':"Fr"
-}
-
-def get(d,key):
-        try:
-            return d[key]
-        except key: 
-            return "no key found"
-        except IndexError:
-            return 'no indexError'
-
-print(get(person,"name"))
-print(get(person,"age"))
+    print('thats not a number!')
+else:
+    print('this is else section')
+finally:
+    print('thisis finally section')
