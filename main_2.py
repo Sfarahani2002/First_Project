0b4a14dc19968ed60b2740ce88ea8a295025f1ea
Saleh <theend.s.2002@gmail.com>
2025-07-07 03:55:11 -0700

    madcircle

--- main_2.py
+++ main_2.py
@@ -1,9 +1,10 @@
-# import random #as rand - R => mokhafaf
-# print(random.randint(1,80)) 
-# myCourses = ['python', 'java', 'c', 'Ai']
-# print(random.choice(myCourses))
+import random 
+print(random.randint(1,80)) 
+myCourses = ['python', 'java', 'c', 'Ai']
+print(random.choice(myCourses))
 #------------------------------------
 from random import randint, choice
 print(randint(1,80))
 myCourses = ['python', 'java', 'c', 'Ai']
 print(choice(myCourses))
+#------------------------------------
