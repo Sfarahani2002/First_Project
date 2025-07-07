d2180acefa85f629e38fd8f4c136a678f678fe6f
Saleh <theend.s.2002@gmail.com>
2025-07-07 12:15:25 -0700

    ProjTermC

--- main_3.py
+++ main_3.py
@@ -1,18 +1,34 @@
 import pyfiglet
-# import termcolor2
+import termcolor2
 from termcolor2 import colored
-# print('saleh')
-# print('-----------------')
-# print(pyfiglet.figlet_format('saleh'))
-# print(termcolor2.colored((pyfiglet.figlet_format('mohammad')),color= 'blue'))
+#---------------------------------------------------------
+print('saleh')
+print('-----------------')
+print(pyfiglet.figlet_format('saleh'))
+print(termcolor2.colored((pyfiglet.figlet_format('mohammad')),color= 'blue'))
 
-#----------------------------------------------
+#---------------------------------------------------------
 valid_colors = ("red", 'green','blue', 'yellow')
 message = input('what would you like to print? : ')
-color = input('what color? : ')
+color= input('what color? : ')
 if color not in valid_colors:
-    color = "blue"
-
+     color = "blue"
+     
 ascii_art = pyfiglet.figlet_format(message)
 ascii_art = colored(ascii_art,color=color)
 print(ascii_art)
+#---------------------------------------------------------
+valid_colors = ("red", 'green','blue', 'yellow')
+def print_art(color,message):
+    if color not in valid_colors:
+        color = "blue"
+
+    ascii_art = pyfiglet.figlet_format(message)
+    ascii_art = colored(ascii_art,color=color)
+    print(ascii_art)
+    
+message = input('what would you like to print? : ')
+color = input('what color? : ')
+print_art(color, message)
+
+#---------------------------------------------------------
