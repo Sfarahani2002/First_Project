43f47151608b5f44390fc524456d1a5cd7946b39
Saleh <theend.s.2002@gmail.com>
2025-07-07 11:23:31 -0700

    module2

---
+++ module.py
@@ -0,0 +1,12 @@
+import termcolor2 
+
+print(termcolor2.colored("python course", color= "blue"))
+print(termcolor2.colored("python course", color= "yellow"))
+print(termcolor2.colored("python course", color= "black"))
+print(termcolor2.colored("python course", color= "red"))
+print(termcolor2.colored("python course", color= "green"))
+print(termcolor2.colored("python course", color= "cyan"))
+print(termcolor2.colored("python course", color= "cyan", on_color= "on_red"))
+print(termcolor2.colored("python course", color= "blue", attrs= ['blink']))
+
+#help(termcolor)
