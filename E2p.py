86b70649dfa55acfdc6b705a2bb669e71d8cb474
Saleh <theend.s.2002@gmail.com>
2025-07-14 04:57:40 -0700

    pycharm1

---
+++ E2p.py
@@ -0,0 +1,11 @@
+import circle
+
+
+number_1 = input("please enter a number: ")
+number_2 = input("please enter a number: ")
+result_1 = int(number_1)
+result_2 = int(number_2)
+sum = result_1 + result_2
+print(f"sum is {sum}")
+
+print(circle.calculateCircleArea(2))
--- first_1.py
+++ first_1.py
@@ -1,3 +1,12 @@
+# import termcolor2
+# import termcolor
+# print(termcolor2.colored("saleh ", color= "cyan"))
+
+import pyfiglet
 import termcolor2
-import termcolor
-print(termcolor2.colored("saleh ", color= "cyan"))
+from termcolor2 import colored
+
+print('saleh')
+print('-----------------')
+print(pyfiglet.figlet_format('saleh'))
+print(termcolor2.colored((pyfiglet.figlet_format('sAleh')),color= 'blue'))
