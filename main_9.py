288010fef595244222db7625fded6558ddae63f8
Saleh <theend.s.2002@gmail.com>
2025-08-03 13:10:39 -0700

    poly3

---
+++ main_9.py
@@ -0,0 +1,30 @@
+class person:
+    def __init__(self, name, family, age, money):
+        self.name = name
+        self.family = family
+        self.age = age
+        self.money = money
+    def __repr__(self):
+        return f'{self.name} {self.family}  {self.age}'
+
+    def __len__(self):
+        return self.age
+
+    def __add__(self, other):
+        return self.money + other.money
+
+    def __mul__(self, other):
+        return self.money * other.money
+
+    def __truediv__(self, other):
+        return self.money / other.money
+
+
+
+person_1 =  person('saleh', 'fr', 23,500)
+person_2 =  person('beni', 'fa', 22, 2000)
+print(person_1)
+print(len(person_1))
+print(person_1 + person_2)
+print(person_1 * person_2)
+print(person_1 / person_2)
