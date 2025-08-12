db2d15d1c326d73fbb6091787df25a26b43ee634
Saleh <theend.s.2002@gmail.com>
2025-08-12 09:48:24 -0700

    Decorator1

--- Decorator.py
+++ Decorator.py
@@ -3,24 +3,42 @@ from random import choice
 def greet(person):
     def get_mood():
         msg = choice(('hello there ', 'go away ', 'goodbye '))
-        return msg
+        res = msg + person
+        return res
 
-    result = get_mood() + person
-    return result
+    return get_mood()
 
-print(greet('saleh'))
+res = greet('saleh')
+print(res)
 
 
-def sum(number, func):
-    total = 0
-    for num in range(1, number + 1):   #[1, 2, 3, 4, 5]
-        total += func(num)
-    return total
 
-def square(x):
-    return x * x
 
-print(sum(5, square))
+
+
+
+
+# def greet(person):
+#     def get_mood():
+#         msg = choice(('hello there ', 'go away ', 'goodbye '))
+#         return msg
+#
+#     result = get_mood() + person
+#     return result
+
+# print(greet('saleh'))
+
+#
+# def sum(number, func):
+#     total = 0
+#     for num in range(1, number + 1):   #[1, 2, 3, 4, 5]
+#         total += func(num)
+#     return total
+#
+# def square(x):
+#     return x * x
+#
+# print(sum(5, square))
 
 # 1 + 2 + 3 + 4 + 5 = 15
 # 1 + 4 + 9 + 16 + 25 = 55
