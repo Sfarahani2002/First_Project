200cc1002ddb9a31cf49db80a5d5d8c727408574
Saleh <theend.s.2002@gmail.com>
2025-08-12 09:39:51 -0700

    Decorator

---
+++ Decorator.py
@@ -0,0 +1,26 @@
+from random import choice
+
+def greet(person):
+    def get_mood():
+        msg = choice(('hello there ', 'go away ', 'goodbye '))
+        return msg
+
+    result = get_mood() + person
+    return result
+
+print(greet('saleh'))
+
+
+def sum(number, func):
+    total = 0
+    for num in range(1, number + 1):   #[1, 2, 3, 4, 5]
+        total += func(num)
+    return total
+
+def square(x):
+    return x * x
+
+print(sum(5, square))
+
+# 1 + 2 + 3 + 4 + 5 = 15
+# 1 + 4 + 9 + 16 + 25 = 55
