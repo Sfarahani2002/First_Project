46650ddd51721d40f2721072b69c99931f7728e6
Saleh <theend.s.2002@gmail.com>
2025-08-16 12:11:02 -0700

    file in py

--- Decorator_2.py
+++ Decorator_2.py
@@ -1,3 +1,4 @@
+
 from time import time
 
 def  speed_test_decorator(func):
---
+++ File.py
@@ -0,0 +1,28 @@
+testTextFile = open("./text.txt")
+print(testTextFile.read())
+testTextFile.seek(0)
+print(testTextFile.read())
+print(testTextFile.read())
+print(testTextFile.read())
+print(testTextFile.read())
+
+textLines = testTextFile.readlines()
+print(textLines)
+
+testTextFile.close()
+print(testTextFile.closed)
+print(testTextFile.closed)
+
+# print("hello my name is saleh") => /n = new line
+
+with open("./text.txt") as file:
+    print(file.read())
+    file.seek(0)
+    print(file.read())
+
+with open("./text.txt", mode='w') as file:
+    file.write(" this is edited text\n")
+
+with open("./text.txt", mode='a') as file:  #=> append
+    file.write(" this is new test text")
+
