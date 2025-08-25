7a5d83e0b29d80a3639fbe7122e1bd165cf1cebc
Saleh <theend.s.2002@gmail.com>
2025-08-25 12:58:24 -0700

    button_1

--- main_11.py
+++ main_11.py
@@ -7,13 +7,14 @@ root.resizable(width=false, height=false)
 
 my_name = StringVar()
 
-
 def print_my_name():
     my_name.set('my name is saleh')
 
-
 btn = button(root, text="show my name!", command=lambda: print_my_name())
 btn.place(x=10, y=10)
+
+label = Label(root, textvariable="my_name")
+label.place(x=10, y=50)
 
 root.mainloop()
 
