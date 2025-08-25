ab2ca0a214813d235110c46fdd839aa705749881
Saleh <theend.s.2002@gmail.com>
2025-08-25 12:48:27 -0700

    button

---
+++ main_11.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = TK()
+root.title("button")
+root.geometry("400x300")
+root.resizable(width=false, height=false)
+
+my_name = StringVar()
+
+
+def print_my_name():
+    my_name.set('my name is saleh')
+
+
+btn = button(root, text="show my name!", command=lambda: print_my_name())
+btn.place(x=10, y=10)
+
+root.mainloop()
+
+
+
+
+
+
