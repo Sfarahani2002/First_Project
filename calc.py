13f5dca8279357c9da7295c320e08f6130eb540a
Saleh <theend.s.2002@gmail.com>
2025-08-29 08:12:40 -0700

    calc

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 
 # ========================== Settings ==================
 root = Tk()
@@ -7,49 +8,97 @@ root.title("calculator")
 root.resizable(width=False, height=False)
 color = 'gray'
 root.configure(bg=color)
+
+# ========================== Variables =====================
+num1 = StringVar()
+num2 = StringVar()
+res_value = StringVar()
+
 # ========================== Frames =====================
-top_first = frame(root, width=400, height=60, bg=color)
-tpo_first.pack(side=TOP)
+top_first = Frame(root, width=400, height=60, bg=color)
+top_first.pack(side=TOP)
 
-top_second = frame(root, width=400, height=60, bg=color)
-tpo_second.pack(side=TOP)
+top_second = Frame(root, width=400, height=60, bg=color)
+top_second.pack(side=TOP)
 
-top_third = frame(root, width=400, height=60, bg=color)
-tpo_third.pack(side=TOP)
+top_third = Frame(root, width=400, height=60, bg=color)
+top_third.pack(side=TOP)
 
-top_forth = frame(root, width=400, height=60, bg=color)
-tpo_forth.pack(side=TOP)
+top_forth = Frame(root, width=400, height=60, bg=color)
+top_forth.pack(side=TOP)
+
+# ========================== Functions =====================
+def errorMsg(ms):
+    if ms == 'error':
+        tkinter.messagebox.showerror('Error','something went wrong' )
+    elif ms == 'division zero error':
+        tkinter.messagebox.showerror('Division Error', 'can not divide by zero')
+
+def plus():
+    try:
+        value = float(num1.get()) + float(num2.get())
+        res_value.set(value)
+    except:
+        errorMsg('error')
+
+def minus():
+    try:
+        value = float(num1.get()) - float(num2.get())
+        res_value.set(value)
+    except:
+        errorMsg('error')
+
+def mul():
+    try:
+        value = float(num1.get()) * float(num2.get())
+        res_value.set(value)
+    except:
+        errorMsg('error')
+
+def div():
+    if num2.get() == '0':
+        errorMsg('division zero error')
+    elif num2.get() != '0':
+        try:
+            value = float(num1.get()) / float(num2.get())
+            res_value.set(value)
+        except:
+            errorMsg('error')
 
 # ========================== Buttons =====================
-btn_plus = Botton(top_third, text="+",width=10, highlightbackground=color)
+btn_plus = Button(top_third, text="+",width=10,
+                  highlightbackground=color, command=lambda: plus())
 btn_plus.pack(side=LEFT, padx=10, pady=10)
 
-btn_minus = Botton(top_third, text="-",width=10, highlightbackground=color)
+btn_minus = Button(top_third, text="-",width=10,
+                   highlightbackground=color, command=lambda: minus())
 btn_minus.pack(side=LEFT, padx=10, pady=10)
 
-btn_mul = Botton(top_third, text="*",width=10, highlightbackground=color)
+btn_mul = Button(top_third, text="*",width=10,
+                 highlightbackground=color, command=lambda: mul())
 btn_mul.pack(side=LEFT, padx=10, pady=10)
 
-btn_div = Botton(top_third, text="/",width=10, highlightbackground=color)
+btn_div = Button(top_third, text="/",width=10,
+                 highlightbackground=color, command=lambda: div())
 btn_div.pack(side=LEFT, padx=10, pady=10)
 
 # ========================== Entries and labels =====================
 Label_first_num = Label(top_first, text="Input Number 1: ", bg=color)
 Label_first_num.pack(side=LEFT, padx=10, pady=10)
 
-first_num = Entry(top_first, highlightbackground=color)
+first_num = Entry(top_first, highlightbackground=color, textvariable=num1)
 first_num.pack(side=LEFT, padx=10, pady=10)
 
 Label_second_num = Label(top_second, text="Input Number 2: ", bg=color)
 Label_second_num.pack(side=LEFT, padx=10, pady=10)
 
-second_num = Entry(top_second, highlightbackground=color)
+second_num = Entry(top_second, highlightbackground=color, textvariable=num2)
 second_num.pack(side=LEFT, padx=10, pady=10)
 
 res = Label(top_forth, text="Result: ", bg=color)
 res.pack(side=LEFT, padx=10, pady=10)
 
-res_num = Entry(top_forth, highlightbackground=color)
+res_num = Entry(top_forth, highlightbackground=color, textvariable=res_value)
 res_num.pack(side=LEFT)
 
 root.mainloop()
