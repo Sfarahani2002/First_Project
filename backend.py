c5c3a12f8fc02f1875e1be99de21fabee292cf25
Saleh <theend.s.2002@gmail.com>
2025-09-02 07:42:31 -0700

    backend2

--- backend.py
+++ backend.py
@@ -22,6 +22,31 @@ def view():
     conn.close()
     return rows
 
+def search(title,author,year,isbn):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=? ",(title,author,year,isbn))
+    rows = cur.fetchall()
+    conn.close()
+
+def delete(id):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,year,isbn,id) )
+    conn.commit()
+    conn.close()
+
 connect()
-insert("python ebook","saleh",2002,2345)
+# insert("python ebook","saleh",2002,2345)
+# delete(1)
+update(1,"python ebook","saleh",2025,11228)
 print(view())
+# print(search(title="ebook"))
+# print(search(author="ben"))
