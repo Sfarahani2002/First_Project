485d3c9cb8bc1bd2002e8379b33c3b9ead177e43
Saleh <theend.s.2002@gmail.com>
2025-09-06 09:37:09 -0700

    wwf2

--- main_12.py
+++ main_12.py
@@ -1,12 +1,19 @@
 import os
+from pathlib import Path
 
 print(os.listdir('./my_files'))
 print(os.path.isfile('./my_files/doc.txt'))
 print(os.path.isdir('./my_files/pictures'))
 
-
 result = os.scandir('./my_files')
 
 for item in result:
     if item.is_file():  # is_dir
+        print(item)
+
+
+directory = Path('./my_files')
+
+for item in directory.iterdir():
+    if item.is_dir():
         print(item)
