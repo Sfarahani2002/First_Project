a87b16c4071971e8cbe65bb5d6b1d8fa8206ac37
Saleh <theend.s.2002@gmail.com>
2025-09-07 05:57:49 -0700

    stat

--- main_12.py
+++ main_12.py
@@ -1,19 +1,27 @@
 import os
+import time
 from pathlib import Path
 
-print(os.listdir('./my_files'))
-print(os.path.isfile('./my_files/doc.txt'))
-print(os.path.isdir('./my_files/pictures'))
+# print(os.listdir('./my_files'))
+# print(os.path.isfile('./my_files/doc.txt'))
+# print(os.path.isdir('./my_files/pictures'))
+#
+# result = os.scandir('./my_files')
+#
+# for item in result:
+#     if item.is_file():  #is_dir
+#         print(item.stat())
+#         print(f'File {item.name}: {time.ctime(item.stat().st_mtime)}')
+#         print(item)
+#
+#
+# directory = Path('./my_files')
+#
+# for item in directory.iterdir():
+#     if item.is_dir():
+#         print(item)
 
-result = os.scandir('./my_files')
-
-for item in result:
-    if item.is_file():  # is_dir
-        print(item)
-
-
-directory = Path('./my_files')
-
-for item in directory.iterdir():
-    if item.is_dir():
-        print(item)
+result = os.stat('./my_files/doc.txt')
+print(result)
+print(result.st_mtime)
+print(time.ctime(result.st_mtime))
