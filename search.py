64c8267828b451eff3c43f9a31241225b46aa755
Saleh <theend.s.2002@gmail.com>
2025-09-07 06:58:56 -0700

    search2

--- search.py
+++ search.py
@@ -1,6 +1,9 @@
 import os
 import pathlib
 import fnmatch
+import glob
+from pathlib import Path
+
 
 for file_name in os.listdir('./test'):
     if fnmatch.fnmatch(file_name, '*[0-9][0-9]*'):
@@ -9,3 +12,11 @@ for file_name in os.listdir('./test'):
         print(file_name)
 
 print(fnmatch.fnmatch('./my_files/data-1.txt', '*.png'))
+
+print(glob.glob('./my_files/*/*[0-9][0-9]*', recursive=True))
+print(glob.glob('./my_files/*[0-9][0-9]*', recursive=True))
+print(glob.glob('**/*[0-9][0-9]*', recursive=True))
+path = Path('./my_files')
+data = path.glob('**/*[0-9][0-9]*')
+print(data)
+print(list(data))
