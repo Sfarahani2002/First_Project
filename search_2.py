2a9f249193576074c1232f27dcb9c580f522a343
Saleh <theend.s.2002@gmail.com>
2025-09-08 10:19:07 -0700

    walkcopy

--- search_2.py
+++ search_2.py
@@ -1,7 +1,17 @@
 import os
+import shutil
+
 
 # for data in os.walk('./my_files', topdown=False): az akhar b aval
 for data in os.walk('./my_files'):
     print(data)
 
+shutil.copy('./my_files/data-1.txt','./new_my_files/new-data-1.txt')
+print(os.stat('./my_files/data-1.txt'))
+print(os.stat('./new_my_files/new-data-1.txt'))
 
+shutil.copy('./my_files/data-2.txt','./new_my_files/new-data-2.txt')
+print(os.stat('./my_files/data-2.txt'))
+print(os.stat('./new_my_files/new-data-2.txt'))
+
+shutil.copytree('./my_files', './backup')
