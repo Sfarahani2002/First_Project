50c8452c9ddfeecd1855a3e47bbcdd22df8c02cd
Saleh <theend.s.2002@gmail.com>
2025-09-08 10:27:38 -0700

    walkMoveRename

--- search_2.py
+++ search_2.py
@@ -3,7 +3,9 @@ import shutil
 
 
 # for data in os.walk('./my_files', topdown=False): az akhar b aval
+# for data in os.walk('./', topdown=False):  all files
 for data in os.walk('./my_files'):
+
     print(data)
 
 shutil.copy('./my_files/data-1.txt','./new_my_files/new-data-1.txt')
@@ -15,3 +17,7 @@ print(os.stat('./my_files/data-2.txt'))
 print(os.stat('./new_my_files/new-data-2.txt'))
 
 shutil.copytree('./my_files', './backup')
+
+shutil.move('./my_files/main_data.txt', './new_my_files/main-data.txt')
+
+os.rename('./my_files/main_data.txt', './new_my_files/new_main_data.txt')
