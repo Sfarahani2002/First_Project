08fb187d501b9bac2ec2129d2fa9326d8e0c6315
Saleh <theend.s.2002@gmail.com>
2025-09-16 13:10:11 -0700

    regex2

--- Regex.py
+++ Regex.py
@@ -31,3 +31,35 @@ import re
 #     print('this is ok')
 
 #endregion
+
+# region set
+#
+# text = 'site'
+# if re.search('si[tdz]e', text):
+#     print('this is ok')
+#
+#endregion
+
+#region range
+
+# text ='s'
+# if re.search('[a-zA-Z]',text):  # [0-9][a-z]
+#     print('this is ok')
+# endregion
+
+#region exclude
+
+# text = 'siue'
+# if re.search('si[^tsz]e', text):
+#     print('this is ok')
+#
+# endregion
+
+#region repeat
+
+# text = '09123456789'
+# if re.search('[0-9]{11}', text):
+#     print('this is ok')
+#
+# endreigion
+
